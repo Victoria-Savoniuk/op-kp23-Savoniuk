bb8b48466e2f15f95a09c38287be54fe7de2c239
Victoria-Savoniuk <114365042+Victoria-Savoniuk@users.noreply.github.com>
2022-10-21 23:50:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Practise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = 10, val = 1, blank, i, j;
+            Console.WriteLine("Pascal's triangle");
+            for (i = 0; i < rows; i++)
+            {
+                for (blank = 1; blank <= rows - i; blank++)
+                    Console.Write("  ");
+
+
+                for (j = 0; j <= i; j++)
+                {
+                    if (j == 0 || i == 0)
+                        val = 1;
+                    else
+                        val = val * (i - j + 1) / j;
+                    Console.Write(val + "   ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
