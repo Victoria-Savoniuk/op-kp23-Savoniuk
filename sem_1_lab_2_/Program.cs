585c3882c6e5d469468f720ada5e2ab021deeb1f
Victoria <victoriasavoniuk1906@gmail.com>
2023-01-08 08:11:22 +0200

    Add prototypes of program

---
+++ sem_1_lab_2_/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Labs2
+{
+    class Percolation
+    {
+        static void Main(string[] args)
+        {
+            Console.WindowHeight = 30;
+            Console.WindowWidth = 70;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear(); 
+
+            int n = 1;
+            int[] matrix = new int[n * n];
+            int row = 0;
+            int col = 0;
+            Console.WriteLine(matrix[row * n + col]);
+        }
+    }
+}
