d18aa82fb0a5e9cdff71a154abccbd6e4c322c08
Victoria <victoriasavoniuk1906@gmail.com>
2023-01-08 08:24:33 +0200

    Test cases

--- sem_1_lab_2_/Program.cs
+++ sem_1_lab_2_/Program.cs
@@ -5,6 +5,20 @@ namespace Labs2
     {
         static void Main(string[] args)
         {
+            /* Test cases:
+             * Open: row = 1; col = 1
+             *       row = 1; col = 2
+             *       row = 2; col = 2
+             *       row = 2; col = 3
+             *       row = 3; col = 3
+             *  Output:
+             *        0    0  -1
+             *        -1   0   0
+             *        -1  -1   0
+             *        
+             *    Number of open sites: 5
+             *    Percolation: true
+             */
             Console.WindowHeight = 30;
             Console.WindowWidth = 70;
             Console.BackgroundColor = ConsoleColor.White;
