d50635a113a4bf2e7f0fcea39c338b18563605fc
Victoria <victoriasavoniuk1906@gmail.com>
2023-03-01 01:03:39 +0200

    Homework2

---
+++ Homework Sem2/Homework2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Homework
+{
+    class Program
+    {
+        static string OUTPUT_FILE_PATH = "C:\\Users\\Віта\\Desktop\\Homework2\\Output.txt";
+        static void Main(string[] args)
+        {
+            string pathName = "C:\\Users\\Віта\\Desktop\\Homework2\\text.txt";
+            string text = File.ReadAllText(pathName);
+            Console.WriteLine(text);
+
+            File.WriteAllText(OUTPUT_FILE_PATH, text);
+
+        }
+    }
+}
