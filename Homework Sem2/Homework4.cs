1ec98d5ada18b5ed4ab8359c9271d8e92ec1f607
Victoria <victoriasavoniuk1906@gmail.com>
2023-03-01 03:30:59 +0200

    Homework4

---
+++ Homework Sem2/Homework4.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Homework
+{
+    class Program
+    {
+        static string OUTPUT_FILE_PATH = "C:\\Users\\Віта\\Desktop\\Homework4\\Output.txt";
+        static void Main(string[] args)
+        {
+            // Зчитати/записати текстовий файл поcимвольно
+            string pathName = "C:\\Users\\Віта\\Desktop\\Homework4\\text.txt";
+            Stream s = new FileStream(pathName, FileMode.Open);
+            int val = 0;
+            char ch;
+
+            while (true)
+            {
+                val = s.ReadByte();
+
+                if (val < 0)
+                {
+                    break;
+                }
+                ch = (char)val;
+                Console.Write(ch);
+                StreamWriter sw = new StreamWriter(OUTPUT_FILE_PATH, true, Encoding.ASCII);
+
+                sw.Write(ch);
+
+                sw.Close();
+
+            }
+
+        }
+    }
+}
