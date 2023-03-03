c8d3b8faef73ea5b74a7caf1ff5236b3059a387b
Victoria <victoriasavoniuk1906@gmail.com>
2023-03-03 05:12:34 +0200

    Add prototypes of program Task 3

---
+++ sem_2_lab_1_/Task 3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "words.txt");
+            string outputPath = Path.Combine(Directory.GetCurrentDirectory(), "sorted words.txt");
+
+            string[] lines = File.ReadAllLines(filePath);
+
+            Sort();
+
+            using (StreamWriter sw = new StreamWriter(outputPath))
+            {
+               
+            }
+        }
+        static int Sort()
+        {
+            return 1;
+        }
+    }
+}
