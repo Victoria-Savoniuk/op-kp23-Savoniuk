256bc1affdb9015ed8c712ff8622a2c49502dd5c
Victoria <victoriasavoniuk1906@gmail.com>
2023-03-03 05:29:48 +0200

    Test cases Task 3

--- sem_2_lab_1_/Task 3.cs
+++ sem_2_lab_1_/Task 3.cs
@@ -11,6 +11,51 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            /*
+             Test case 1:                                                 Test case 2:
+                                          Output:                                       Output:
+            Book                        Art                             Book            Art
+            Mail                        Bed                             Book            Art
+            Window                      Birds                           Book            Art
+            Coffee                      Book                            Art             Book
+            Milk                        Bread                           Art             Book
+            Bread                       Bus                             Art             Book
+            Mirror                      Candles 
+            Park                        Coffee 
+            Home                        Cookies 
+            Bus                         Day 
+            Train                       Dinner 
+            Sun                         Flowers 
+            Water                       Glasses 
+            Music                       Hands 
+            Phone                       Health 
+            Flowers                     Home 
+            Snow                        Love 
+            Hands                       Mail 
+            Bed                         Milk 
+            Table                       Mirror 
+            Radio                       Money 
+            Sport                       Music 
+            Sound                       Park 
+            Art                         Phone 
+            Money                       Pillow
+            Street                      Potato 
+            Towel                       Radio 
+            Health                      Sea 
+            Potato                      Snow 
+            Cookies                     Sound 
+            Birds                       Sport 
+            Tea                         Street 
+            Candles                     Sun 
+            Dinner                      Table 
+            Glasses                     Tea 
+            Day                         Towel 
+            Sea                         Train 
+            Love                        Water 
+            Wind                        Wind 
+            Pillow                      Window 
+             */
+
             string filePath = Path.Combine(Directory.GetCurrentDirectory(), "words.txt");
             string outputPath = Path.Combine(Directory.GetCurrentDirectory(), "sorted words.txt");
 
