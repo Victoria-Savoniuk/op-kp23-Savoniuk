ed97c49774a9cbbd029297d1859e1541e09294e2
Victoria <victoriasavoniuk1906@gmail.com>
2023-03-03 03:26:38 +0200

    Add prototypes of program Task 1

---
+++ sem_2_lab_1_/Task 1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace Files
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathName = Path.Combine(Directory.GetCurrentDirectory(), "sem 2 lab 1 task 1.txt");
+
+            FileStream fs = File.Create(pathName);
+            fs.Close();
+
+            StreamWriter sw = new StreamWriter(pathName);
+           
+            sw.Close();
+
+            string text = File.ReadAllText(pathName);
+            Console.WriteLine(text);
+        }
+    }
+}
