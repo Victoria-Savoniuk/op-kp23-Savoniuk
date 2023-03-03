bd891607c11dc47e9fde329696c54b304a34c528
Victoria <victoriasavoniuk1906@gmail.com>
2023-03-03 04:37:59 +0200

    Add prototypes of program Task 4

---
+++ sem_2_lab_1_/Task 4.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Список.csv");
+
+            
+            string[] lines = File.ReadAllLines(filePath);
+            int length = lines.Length * 3;
+            string[] Mark = new string[length];
+
+            Split();
+            
+            int n = 0;
+
+            for (int i = 2; i < Mark.Length; i += 3)
+            {
+                if (Convert.ToInt32(Mark[i]) < 60)
+                {
+                    n++;
+                    Console.WriteLine($"{Mark[i - 2]} {Mark[i - 1]} - {Mark[i]}");
+                }
+            }
+            if (n == 0)
+            {
+                Console.WriteLine("Таких студентів немає");
+            }
+        }
+        static void Split()
+        {
+            
+        }
+    }
+}
