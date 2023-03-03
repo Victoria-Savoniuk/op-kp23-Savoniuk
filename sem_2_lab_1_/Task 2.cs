330a007461465781976c1e21f41c144b3690366e
Victoria <victoriasavoniuk1906@gmail.com>
2023-03-03 03:51:47 +0200

    Add prototypes of program Task 2

---
+++ sem_2_lab_1_/Task 2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace Files
+{
+    class Program
+    {
+
+        static string OUTPUT_FILE_PATH = Path.Combine(Directory.GetCurrentDirectory(), "max.txt");
+        static void Main(string[] args)
+        {
+            string pathName = Path.Combine(Directory.GetCurrentDirectory(), "sem 2 lab 1 task 2.txt");
+
+            FileStream fs = File.Create(pathName);
+            fs.Close();
+
+            StreamWriter sr = new StreamWriter(pathName);
+            sr.Close();
+
+            int max = 0;
+            
+            StreamReader s = new StreamReader(pathName);
+            s.Close();
+
+            Console.WriteLine(max);
+
+            StreamWriter sw = new StreamWriter(OUTPUT_FILE_PATH);
+            sw.WriteLine(max);
+            sw.Close();
+        }
+    }
+}
