b7c19964840110cbf45271496a8f0778416fa47a
Victoria <victoriasavoniuk1906@gmail.com>
2023-03-10 00:11:51 +0200

    Final program Task 5

---
+++ sem_2_lab_1_/Task 5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace Labs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "text.txt");
+
+            StreamWriter sw = File.AppendText(filePath);
+            sw.Write(" ");
+            sw.Close();
+
+            StreamReader sr = new StreamReader(filePath);
+
+            sr.Close();
+        }
+    }
+}
