bdd52c8140dc36e7e6ee5d1012dc1f4fd9564424
Victoria <victoriasavoniuk1906@gmail.com>
2023-03-09 22:43:38 +0200

    Test cases Task 6

--- sem_2_lab_1_/Task 6.cs
+++ sem_2_lab_1_/Task 6.cs
@@ -8,6 +8,25 @@ namespace Labs
     {
         static void Main(string[] args)
         {
+            /*
+            Test case1:
+              Victoria;Savoniuk;96
+              Denys;Tabachuk;98
+              Valeria;Kovalchuk;53
+              Nazar;Myhlovets;62
+              Valeria;Eismont;78
+              Output:
+               Victoria Savoniuk 96
+               Denys Tabachuk 98
+            Test case2:
+              Victoria;Savoniuk;93
+              Denys;Tabachuk;95
+              Valeria;Kovalchuk;53
+              Nazar;Myhlovets;62
+              Valeria;Eismont;78
+              Output:
+              There are no students with a score above 95
+             */
             string filePath = Path.Combine(Directory.GetCurrentDirectory(), "Список.csv");
             string inputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "input.bin");
             string outputFilePath = Path.Combine(Directory.GetCurrentDirectory(), "output.bin");
