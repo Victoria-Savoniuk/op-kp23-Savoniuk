bbb06202c9a98b8036d6894d26040c2d6d05b08f
Victoria <victoriasavoniuk1906@gmail.com>
2023-03-10 00:46:26 +0200

    Test cases Task 5

--- sem_2_lab_1_/Task 5.cs
+++ sem_2_lab_1_/Task 5.cs
@@ -7,6 +7,29 @@ namespace Labs
     {
         static void Main(string[] args)
         {
+            /*
+             Test case:
+             Victoria Savoniuk
+             Hello World!!!
+             Hello/Hi
+             &Victoria.
+             Everyone sees the world in one's own way
+            Output:
+            Victoria: 2
+            Savoniuk: 1
+            Hello: 2
+            World: 1
+            Hi: 1
+            Everyone: 1
+            sees: 1
+            the: 1
+            world: 1
+            in: 1
+            one's: 1
+            own: 1
+            way: 1
+             */
+
             string filePath = Path.Combine(Directory.GetCurrentDirectory(), "text.txt");
 
             StreamWriter sw = File.AppendText(filePath);
