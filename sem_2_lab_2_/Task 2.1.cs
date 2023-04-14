d7e61afa7ddfc12d026f25f08c8de0512cdd5286
Victoria <victoriasavoniuk1906@gmail.com>
2023-04-14 18:09:36 +0300

    Add prototypes of program Task 2.1

---
+++ sem_2_lab_2_/Task 2.1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            Employee[] employees = new Employee[num];
+            SalaryReport report = new SalaryReport(employees);
+        }
+    }
+    class Employee
+    {
+        public string _lastName;
+        public double _salary;
+        public double _withheld;
+        public double _given;
+        public Employee(string lastName, double salary, double withheld)
+        {
+            _lastName = lastName;
+            _salary = salary;
+            _withheld = withheld;
+            _given = salary - withheld;
+        }
+    }
+
+    class SalaryReport
+    {
+        private Employee[] _employees;
+        public SalaryReport(Employee[] employees)
+        {
+            _employees = employees;
+        }
+    }
+}
