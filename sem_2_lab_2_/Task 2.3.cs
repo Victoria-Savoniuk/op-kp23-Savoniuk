0dea30d55ea78da6c2be50fe4d7778cc0cdf3a33
Victoria <victoriasavoniuk1906@gmail.com>
2023-04-14 18:46:49 +0300

    Test cases Task 2.3

--- sem_2_lab_2_/Task 2.3.cs
+++ sem_2_lab_2_/Task 2.3.cs
@@ -3,6 +3,25 @@ namespace Vessel
 {
     public class Program
     {   
+        /*Test cases:
+         *    Case 1:
+         *  Input:
+         *  num = 0;
+         *  Output:
+         *  Такого номера судна немає. Повторіть спробу.
+         *      Case 2:
+         *  Input:
+         *  num = 1;
+         *  Output:
+         *  Підготовка парусника до руху
+         *  Парусник пливе
+         *      Case 3:
+         *  Input:
+         *  num = 2;
+         *  Output:
+         *  Підготовка підводного човна до руху
+         *  Підводний човен пірнає
+         */
         public static void Main(string[] args)
         {
             IVessel vessel1 = new SailingVessel();
