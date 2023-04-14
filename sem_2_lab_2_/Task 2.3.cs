04bef0da8928522c7353f083073437675e4fdaf8
Victoria <victoriasavoniuk1906@gmail.com>
2023-04-14 18:52:08 +0300

    Final program Task 2.3

--- sem_2_lab_2_/Task 2.3.cs
+++ sem_2_lab_2_/Task 2.3.cs
@@ -24,9 +24,48 @@ namespace Vessel
          */
         public static void Main(string[] args)
         {
-            IVessel vessel1 = new SailingVessel();
-            IVessel vessel2 = new Submarine();
-            
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("{0,-15}{1,-20}","№ судна","Вид судна");
+            Console.WriteLine("{0,-15}{1,-20}","1","Парусник");
+            Console.WriteLine("{0,-15}{1,-20}","2","Підводний човен");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine();
+            Console.WriteLine("Виберіть номер судна");
+            int num = 0;
+            num = Convert.ToInt32(Console.ReadLine());
+            while (num != 1 && num != 2)
+            {
+                Console.WriteLine("Такого номера судна немає. Повторіть спробу.");
+                Console.WriteLine("Виберіть номер судна");
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            string commands = "";
+            if(num == 1)
+            {
+                IVessel vessel1 = new SailingVessel();
+                vessel1.PrepareToMovement();
+                Console.WriteLine("Бажаєте розпочати рух парусника? Якщо так, то натисніть +");
+                commands = Console.ReadLine();
+                while (commands != "+")
+                {
+                    Console.WriteLine("Такої команди не існує, щоб розпочати рух парусника натисніть +");
+                    commands = Console.ReadLine();
+                }
+                vessel1.Move();
+            }
+            if (num == 2)
+            {
+                IVessel vessel2 = new Submarine();
+                vessel2.PrepareToMovement();
+                Console.WriteLine("Бажаєте розпочати рух підводного човна? Якщо так, то натисніть +");
+                commands = Console.ReadLine();
+                while (commands != "+")
+                {
+                    Console.WriteLine("Такої команди не існує, щоб розпочати рух підводного човна натисніть +");
+                    commands = Console.ReadLine();
+                }
+                vessel2.Move();
+            }
         }
     }
     public interface IVessel
