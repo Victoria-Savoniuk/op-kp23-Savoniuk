fad9f6b5a0511ab861d18233d2efb5c000b67850
Victoria <victoriasavoniuk1906@gmail.com>
2023-04-14 18:45:42 +0300

    Add prototypes of program Task 2.3

---
+++ sem_2_lab_2_/Task 2.3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Vessel
+{
+    public class Program
+    {   
+        public static void Main(string[] args)
+        {
+            IVessel vessel1 = new SailingVessel();
+            IVessel vessel2 = new Submarine();
+            
+        }
+    }
+    public interface IVessel
+    {
+    }
+    public class SailingVessel : IVessel
+    {
+    }
+    public class Submarine : IVessel
+    {
+    }
+}
