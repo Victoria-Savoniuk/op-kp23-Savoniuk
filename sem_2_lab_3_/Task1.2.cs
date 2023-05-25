be02f44322db1da9a87fba8bb5573b4c2e8428aa
Victoria <victoriasavoniuk1906@gmail.com>
2023-05-25 23:41:42 +0300

    Implementation of Deque class methods

--- sem_2_lab_3_/Task1.2.cs
+++ sem_2_lab_3_/Task1.2.cs
@@ -12,12 +12,21 @@ namespace Lab3
 
         public Deque()
         {
- 
+            capacity = 30;
+            array = new Item[capacity];
+            head = -1;
+            tail = -1;
+            size = 0;
+            current = head;
         }
 
         // Is the deque empty?
         public bool IsEmpty()
         {
+            if (Size() != 0)
+            {
+                return false;
+            }
             return true;
         }
 
@@ -30,31 +39,124 @@ namespace Lab3
         // Add the item to the front
         public void AddFirst(Item item)
         {
+            if (item == null)
+            {
+                throw new ArgumentNullException("Item cannot be null.");
+            }
 
+            if (size == capacity)
+            {
+                Resize();
+            }
+
+            if (head == -1)
+            {
+                head = tail = capacity / 2;
+            }
+            else if (head == 0)
+            {
+                Resize();
+                AddFirst(item);
+            }
+            else
+            {
+                head = head - 1;
+            }
+            array[head] = item;
+            size++;
+            current = head;
         }
 
         // Add the item to the back
         public void AddLast(Item item)
         {
+            if (item == null)
+            {
+                throw new ArgumentNullException("Item cannot be null.");
+            }
 
+            if (size == capacity)
+            {
+                Resize();
+            }
+
+            if (tail == -1)
+            {
+                head = tail = capacity / 2;
+            }
+            else if (tail == capacity)
+            {
+                Resize();
+                AddLast(item);
+            }
+            else
+            {
+                tail = tail + 1;
+            }
+            array[tail] = item;
+            size++;
+            current = head;
         }
 
         // Remove and return the item from the front
         public Item RemoveFirst()
         {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Deque is empty");
+            }
+
             Item value = array[head];
+
+            if (head == tail)
+            {
+                head = tail = -1;
+            }
+            else
+            {
+                head = head + 1;
+            }
+
+            size--;
+            current = head;
             return value;
         }
 
         // Remove and return the item from the back
         public Item RemoveLast()
         {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Deque is empty");
+            }
+
             Item value = array[tail];
+
+            if (head == tail)
+            {
+                head = tail = -1;
+            }
+            else
+            {
+                tail = tail - 1;
+            }
+            size--;
+            current = head;
             return value;
         }
         private void Resize()
         {
-
+            Item[] newArray = new Item[capacity * 2];
+            int j = 0;
+            for (int i = head; i <= tail; i++)
+            {
+                newArray[i] = array[j];
+                j++;
+            }
+            array = newArray;
+            capacity = capacity * 2;
+            head = 0;
+            tail = size - 1;
         }
         public IIterator<Item> iterator()
         {
