c6118a1311255725d7137241c7f510e7641255c6
Victoria <victoriasavoniuk1906@gmail.com>
2023-05-25 17:47:19 +0300

    Implementation of Main

--- sem_2_lab_3_/Task2.1.cs
+++ sem_2_lab_3_/Task2.1.cs
@@ -146,6 +146,188 @@ namespace Lab3
         public static void Main(string[] args)
         {
             RandomizedQueue<int> queue = new RandomizedQueue<int>();
+            IIterator<int> it = queue;
+
+            string[] options = { "Add item to Randomized Queue", "Remove and return a random element from the Randomized Queue",
+            "Return a random element from the Randomized Queue (but without removing it)", "Show Randomized Queue", "Unit Test" };
+            int selectedIndex = 0;
+
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("Use the arrow keys to select a command:");
+                for (int i = 0; i < options.Length; i++)
+                {
+                    if (i == selectedIndex)
+                    {
+                        Console.Write("> ");
+                    }
+                    else
+                    {
+                        Console.Write("  ");
+                    }
+                    Console.WriteLine(options[i]);
+                }
+
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        selectedIndex--;
+                        if (selectedIndex < 0)
+                        {
+                            selectedIndex = options.Length - 1;
+                        }
+                        break;
+                    case ConsoleKey.DownArrow:
+                        selectedIndex++;
+                        if (selectedIndex >= options.Length)
+                        {
+                            selectedIndex = 0;
+                        }
+                        break;
+                    case ConsoleKey.Enter:
+                        Console.Clear();
+                        if (options[selectedIndex] == "Add item to Randomized Queue")
+                        {
+                            while (true)
+                            {
+                                Console.WriteLine("The size of the deque: " + queue.Size());
+                                Console.WriteLine("Randomized Queue items: ");
+                                showIterator(queue);
+                                Console.WriteLine("Enter the element you want to add:");
+                                int item = Convert.ToInt32(Console.ReadLine());
+                                queue.Enqueue(item);
+                                Console.WriteLine("Size of deque after addition: " + queue.Size());
+                                Console.WriteLine("Deque items after adding: ");
+                                showIterator(queue);
+                                Console.WriteLine("To go back press Esc");
+                                int esc = ' ';
+                                esc = (int)Console.ReadKey().Key;
+                                while (esc != 27)
+                                {
+                                    esc = (int)Console.ReadKey().Key;
+                                }
+                                Console.Clear();
+                                break;
+
+                            }
+
+                        }
+                        else if (options[selectedIndex] == "Remove and return a random element from the Randomized Queue")
+                        {
+                            while (true)
+                            {
+                                if(queue.Size() == 0)
+                                {
+                                    Console.WriteLine("Randomized Queue is empty");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Size of Randomized Queue before deletion: " + queue.Size());
+                                    Console.WriteLine("Randomized Queue items: ");
+                                    showIterator(queue);
+                                    int num = queue.Dequeue();
+                                    Console.WriteLine("A random element has been removed: " + num);
+                                    Console.WriteLine("Size of Randomized Queue after deletion: " + queue.Size());
+                                    Console.WriteLine("Randomized Queue items after deletion: ");
+                                    showIterator(queue);
+                                }
+                                Console.WriteLine("To go back press Esc");
+                                int esc = ' ';
+                                esc = (int)Console.ReadKey().Key;
+                                while (esc != 27)
+                                {
+                                    esc = (int)Console.ReadKey().Key;
+                                }
+                                Console.Clear();
+                                break;
+                            }
+                        }
+                        else if (options[selectedIndex] == "Return a random element from the Randomized Queue (but without removing it)")
+                        {
+                            while (true)
+                            {
+                                Console.Clear();
+                                if (queue.Size() == 0)
+                                {
+                                    Console.WriteLine("Randomized Queue is empty");
+                                }
+                                else
+                                {
+                                    Console.WriteLine("The size of the Randomized Queue: " + queue.Size());
+                                    Console.WriteLine("Randomized Queue items: ");
+                                    showIterator(queue);
+                                    int num = queue.Sample();
+                                    Console.WriteLine("Random element: " + num);
+                                }  
+                                Console.WriteLine("To go back press Esc");
+                                int esc = ' ';
+                                esc = (int)Console.ReadKey().Key;
+                                if (esc == 27)
+                                {
+                                    Console.Clear();
+                                    break;
+                                }
+                                Console.Clear();
+                            }
+                        }
+                        else if (options[selectedIndex] == "Show Randomized Queue")
+                        {
+                            while (true)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("The size of the Randomized Queue: " + queue.Size());
+                                Console.WriteLine("Randomized Queue items: ");
+                                showIterator(queue);
+                                Console.WriteLine("To go back press Esc");
+                                int esc = ' ';
+                                esc = (int)Console.ReadKey().Key;
+                                while (esc != 27)
+                                {
+                                    esc = (int)Console.ReadKey().Key;
+                                }
+                                Console.Clear();
+                                break;
+                            }
+                        }
+                        else if (options[selectedIndex] == "Unit Test")
+                        {
+                            while (true)
+                            {
+                                Console.Clear();
+                                UnitTest();
+                                Console.WriteLine("To go back press Esc");
+                                int esc = ' ';
+                                esc = (int)Console.ReadKey().Key;
+                                while (esc != 27)
+                                {
+                                    esc = (int)Console.ReadKey().Key;
+                                }
+                                Console.Clear();
+                                break;
+                            }
+                        }
+                        break;
+                }
+            }
+            static void showIterator(RandomizedQueue<int> deque)
+            {
+                IIterator<int> it = deque.iterator();
+
+                if (deque.Size() == 0)
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    while (it.HasNext)
+                    {
+                        int num = it.MoveNext();
+                        Console.WriteLine(num);
+                    }
+                }
+            }
         }
     }
 }
